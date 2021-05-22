a0fb7017c69c444d8d7427b16d5dcfa149523e89
Sam-Shorty <sam.shorty00@gmail.com>
2021-05-22 11:23:27 +0200

    io and service

---
+++ Fingerprint/lib/io.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+
+import time
+import os
+from pyfingerprint.pyfingerprint import PyFingerprint
+
+def add_finger(f):
+    try:
+        print("Appoggiare il dito...")
+        while ( f.readImage() == False ):
+            pass
+        f.convertImage(0x01)
+
+        print("Rimuovere il dito...")
+        time.sleep(2)
+        print("Appoggia lo stesso dito...")
+        while ( f.readImage() == False ):
+            pass
+        f.convertImage(0x02)
+
+        if ( f.compareCharacteristics() == 0 ):
+            print("Le impronte non combaciano")
+            return
+        
+        f.createTemplate()
+        positionNumber = f.storeTemplate()
+        print("Impronta aggiunta con successo!")
+        print("Posizione impronta: " + str(positionNumber))
+
+    except Exception as e:
+        print("Operazione fallita!")
+        print('Exception message: ' + str(e))
+
+def remove_finger(f):
+    try:
+        tableIndex = f.getTemplateIndex(0)
+
+        print("Posizioni occupate:")
+        for i in range(0, len(tableIndex)):
+            if (tableIndex[i]):
+                print(str(i))
+        
+        pos = input("Quale impronta vuoi eliminare? ")
+        pos = int(pos)
+
+        f.deleteTemplate(pos)
+
+    except Exception as e:
+        print("Operazione fallita!")
+        print('Exception message: ' + str(e))
+
+def clear_finger(f):
+    tableIndex = f.getTemplateIndex(0)
+    for i in range(0, len(tableIndex)):
+        if (tableIndex[i]):
+            f.deleteTemplate(i)
+
+def info(f):
+    try:
+        print('Spazi occupati: ' + str(f.getTemplateCount()) +'/'+ str(f.getStorageCapacity()))
+
+    except Exception as e:
+        print("Operazione fallita!")
+        print('Exception message: ' + str(e))
+
+## Tries to initialize the sensor
+try:
+    f = PyFingerprint('/dev/ttyS0', 57600, 0xFFFFFFFF, 0x00000000)
+    if ( f.verifyPassword() == False ):
+        raise ValueError('Wrong fingerprint password!')
+
+except Exception as e:
+    print('Error during fingerprint initialization!')
+    print('Exception message: ' + str(e))
+    exit(1)
+
+while True:
+    print("What will you do?")
+    print("0) Exit")
+    print("1) Add finger")
+    print("2) Remove finger")
+    print("3) Clear finger memory")
+    print("4) Info")
+
+    chose = input("Inserire scelta...\n")
+    chose = int(chose)
+
+    if (chose == 0):
+        exit(0)
+    elif (chose == 1):
+        add_finger(f)
+    elif (chose == 2):
+        remove_finger(f)
+    elif (chose == 3):
+        clear_finger(f)
+    elif (chose == 4):
+        info(f)
---
+++ Fingerprint/system/fingerlogin.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+
+import time
+import os
+from pyfingerprint.pyfingerprint import PyFingerprint
+
+## Tries to initialize the sensor
+try:
+    f = PyFingerprint('/dev/ttyS0', 57600, 0xFFFFFFFF, 0x00000000)
+    if ( f.verifyPassword() == False ):
+        raise ValueError('Wrong fingerprint password!')
+
+except Exception as e:
+    print('Error during fingerprint initialization!')
+    print('Exception message: ' + str(e))
+    exit(1)
+
+rows, columns = os.popen('stty size', 'r').read().split()
+
+while(True):
+    try:
+        ## Wait that finger is read
+        state = 10
+        while ( f.readImage() == False ):
+            if (state != 0):
+                state = 0
+                os.system('clear')
+
+                cont = 0
+                while cont < (int(rows)/2):
+                    print()
+                    cont = cont + 1
+                cont = 0
+                while cont < ((int(columns)/2) - 16):
+                    print(' ', end = '')
+                    cont = cont + 1
+                
+                print("Appoggiare il dito sul sensore...")
+            pass
+
+        ## Converts read image to characteristics and stores it in charbuffer 1
+        f.convertImage(0x01)
+
+        ## Searchs template
+        result = f.searchTemplate()
+
+        if (result[0] == -1 ):
+            if (state != 1):
+                state = 1
+                os.system('clear')
+
+                cont = 0
+                while cont < (int(rows)/2):
+                    print()
+                    cont = cont + 1
+                cont = 0
+                while cont < ((int(columns)/2) - 10):
+                    print(' ', end = '')
+                    cont = cont + 1
+
+                print('Impronta non trovata')
+            pass
+        else:
+            #funzione di login
+            os.system('clear')
+
+            cont = 0
+            while cont < (int(rows)/2):
+                print()
+                cont = cont + 1
+            cont = 0
+            while cont < ((int(columns)/2) - 13):
+                print(' ', end = '')
+                cont = cont + 1
+
+            print("Impronta trovata, login...")
+            os.system('login -f -p pi')
+            #os.system('startx')
+            #os.system('systemctl start lightdm')
+            os.system('clear')
+            exit(0)
+    
+    except Exception as e:
+        print('Errore: ')
+        print('Exception message: ' + str(e))
+        exit(1)
