564d8c52bb6a7e16c63be7321d00a407ab402462
Sam-Shorty <sam.shorty00@gmail.com>
2021-05-25 00:47:41 +0200

    Update

--- Fingerprint/lib/io.py
+++ Fingerprint/lib/io.py
@@ -1,69 +1,77 @@
 #!/usr/bin/python3
 
 import time
-import os
 from pyfingerprint.pyfingerprint import PyFingerprint
 
+#Function to add a new finger in the sensor
 def add_finger(f):
     try:
-        print("Appoggiare il dito...")
+        #Reading the fingerprint
+        print('Appoggiare il dito...')
         while ( f.readImage() == False ):
             pass
         f.convertImage(0x01)
 
-        print("Rimuovere il dito...")
+        #Reading for the second time
+        print('Rimuovere il dito...')
         time.sleep(2)
-        print("Appoggia lo stesso dito...")
+        print('Appoggia lo stesso dito...')
         while ( f.readImage() == False ):
             pass
         f.convertImage(0x02)
 
+        #COmparing the two fingerprint - if not equal return
         if ( f.compareCharacteristics() == 0 ):
-            print("Le impronte non combaciano")
+            print('Le impronte non combaciano')
             return
         
+        #Adding the fingerprint in the sensor memory
         f.createTemplate()
         positionNumber = f.storeTemplate()
-        print("Impronta aggiunta con successo!")
-        print("Posizione impronta: " + str(positionNumber))
+        print('Impronta aggiunta con successo!')
+        print('Posizione impronta: ' + str(positionNumber))
 
     except Exception as e:
-        print("Operazione fallita!")
+        print('Operazione fallita!')
         print('Exception message: ' + str(e))
 
+#Function to remove finger from the sensor
 def remove_finger(f):
     try:
+        #Print the list of finger stored
         tableIndex = f.getTemplateIndex(0)
-
-        print("Posizioni occupate:")
+        print('Posizioni occupate:')
         for i in range(0, len(tableIndex)):
             if (tableIndex[i]):
                 print(str(i))
         
-        pos = input("Quale impronta vuoi eliminare? ")
+        #Selecting and delete the finger from the sensor memory
+        pos = input('Quale impronta vuoi eliminare? ')
         pos = int(pos)
-
         f.deleteTemplate(pos)
 
     except Exception as e:
-        print("Operazione fallita!")
+        print('Operazione fallita!')
         print('Exception message: ' + str(e))
 
+#Function to delete all the finger in the sensor
 def clear_finger(f):
+    #Cicle to delete all the finger one by one
     tableIndex = f.getTemplateIndex(0)
     for i in range(0, len(tableIndex)):
         if (tableIndex[i]):
             f.deleteTemplate(i)
 
+#Function to print information about the sensor
 def info(f):
     try:
         print('Spazi occupati: ' + str(f.getTemplateCount()) +'/'+ str(f.getStorageCapacity()))
 
     except Exception as e:
-        print("Operazione fallita!")
+        print('Operazione fallita!')
         print('Exception message: ' + str(e))
 
-## Tries to initialize the sensor
+#Tries to initialize the sensor
 try:
     f = PyFingerprint('/dev/ttyS0', 57600, 0xFFFFFFFF, 0x00000000)
     if ( f.verifyPassword() == False ):
@@ -75,24 +83,25 @@ except Exception as e:
     exit(1)
 
 while True:
-    print("What will you do?")
-    print("0) Exit")
-    print("1) Info")
-    print("2) Add finger")
-    print("3) Remove finger")
-    print("4) Clear finger memory")
+    #Printing the menu
+    print('What will you do?')
+    print('0) Exit')
+    print('1) Info')
+    print('2) Add finger')
+    print('3) Remove finger')
+    print('4) Clear finger memory')
     
+    #Select and execute the choose
+    choose = input('Inserire scelta...\n')
+    choose = int(choose)
 
-    chose = input("Inserire scelta...\n")
-    chose = int(chose)
-
-    if (chose == 0):
+    if (choose == 0):
         exit(0)
-    elif (chose == 1):
+    elif (choose == 1):
         info(f)
-    elif (chose == 2):
+    elif (choose == 2):
         add_finger(f)
-    elif (chose == 3):
+    elif (choose == 3):
         remove_finger(f)
-    elif (chose == 4):
+    elif (choose == 4):
         clear_finger(f)
